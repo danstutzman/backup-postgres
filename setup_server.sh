1d01fb1bf27e27cbfccd64418a51bedc36d20a45
Dan Stutzman <dan@danstutzman.com>
2016-04-13 16:51:16 -0600

    Un-hard-code IAM_USERNAME and BUCKET_NAME

--- setup_server.sh
+++ setup_server.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -ex
 
+IAM_USERNAME=backup-postgres-appendonly
+BUCKET_NAME=backup-postgres-danstutzman
 
-AWS_ACCESS_KEY_ID=$(cat aws_creds/backup-postgres-appendonly.accesskey.json | python -c 'import json,sys; obj=json.load(sys.stdin); print obj["AccessKey"]["AccessKeyId"]')
 if [ `gcloud compute instances list postgres-master | wc -l` != "2" ]; then
   gcloud compute instances create postgres-master \
     --machine-type g1-small \
     --image ubuntu-14-04
 fi
+
+AWS_ACCESS_KEY_ID=$(cat aws_creds/$IAM_USERNAME.accesskey.json | python -c 'import json,sys; obj=json.load(sys.stdin); print obj["AccessKey"]["AccessKeyId"]')
 if [ "$AWS_ACCESS_KEY_ID" == "" ]; then
   echo 1>&2 "Empty AWS_ACCESS_KEY_ID"
   exit 1
 fi
 
-AWS_SECRET_ACCESS_KEY=$(cat aws_creds/backup-postgres-appendonly.accesskey.json | python -c 'import json,sys; obj=json.load(sys.stdin); print obj["AccessKey"]["SecretAccessKey"]')
+AWS_SECRET_ACCESS_KEY=$(cat aws_creds/$IAM_USERNAME.accesskey.json | python -c 'import json,sys; obj=json.load(sys.stdin); print obj["AccessKey"]["SecretAccessKey"]')
 if [ "$AWS_SECRET_ACCESS_KEY" == "" ]; then
   echo 1>&2 "Empty AWS_SECRET_ACCESS_KEY"
   exit 1
@@ -41,10 +44,10 @@ diff /etc/postgresql/9.3/main/postgresql.conf.bak /etc/postgresql/9.3/main/postg
 sudo service postgresql restart
 
 sudo mkdir -p /etc/wal-e.d/env
-echo "$AWS_ACCESS_KEY_ID"             | sudo tee /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
-echo "$AWS_SECRET_ACCESS_KEY"         | sudo tee /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
-echo s3://backup-postgres-danstutzman | sudo tee /etc/wal-e.d/env/WALE_S3_PREFIX
-echo us-east-1                        | sudo tee /etc/wal-e.d/env/AWS_REGION
+echo "$AWS_ACCESS_KEY_ID"     | sudo tee /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
+echo "$AWS_SECRET_ACCESS_KEY" | sudo tee /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
+echo s3://$BUCKET_NAME        | sudo tee /etc/wal-e.d/env/WALE_S3_PREFIX
+echo us-east-1                | sudo tee /etc/wal-e.d/env/AWS_REGION
 sudo chown -R root:postgres /etc/wal-e.d
 
 sudo sudo -u postgres envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/9.3/main
